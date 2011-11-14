c9f9cc5ab968894733e346229b52e909714aa9b8
ertugrul Koksal <ekoksal7@gmail.com>
2011-11-14 18:18:27 +0200

    sTag class

---
+++ app/models/STag.java
@@ -0,0 +1,30 @@
+package models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+@Entity
+public class STag extends Model{
+
+	@Required
+	public String text;
+	
+	/*@ManyToOne(fetch = FetchType.LAZY,
+	           cascade = { CascadeType.PERSIST,
+            CascadeType.MERGE })
+	public Task task;*/
+	
+	@ManyToOne
+	public Service service;
+	
+	public STag(String text) {
+        this.text = text;
+    }
+	
+}
