59daf5f58c53d622d30ca0551f57cf02d3886512
ce <cihan.ekici1@boun.edu.tr>
2011-12-19 00:07:19 +0200

    Make cosmetic changes around slots.

--- public/javascripts/commons.js
+++ public/javascripts/commons.js
@@ -1,3 +1,10 @@
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
+
+function leftPadWithZeros(s, l) {
+    while (s.length < l) {
+        s = '0' + s;
+    }
+    return s;
+}
